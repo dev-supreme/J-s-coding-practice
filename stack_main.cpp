d796dffb977bee98034e2e19a50b3167630fdb27
JayLee2017 <dlwodls10@naver.com>
2019-02-04 18:21:04 +0900

    Add files via upload
    원상태로 복구방법을 못 찾아서 이전 버전을 다시 올림.

--- stack_main.cpp
+++ stack_main.cpp
@@ -14,18 +14,18 @@ using namespace std;
 typedef struct node
 {
     int nData;
-    node* pNext;
+    node* pPrev;
 }node;
 
 
-node* input;
-node* output;
-const int QUEUE_SIZE = 5;
-int currentQueueSize = 0;
+node* top;
+const int STACK_SIZE = 5;
+int currentStackSize = 0;
 
 
-void push(int, node**, node**);
+void push(int, node**);
 int pop(node**);
+//int popAllStack(void);
 
 
 
@@ -33,86 +33,73 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     
     
-    push(11, &input, &output);
-    push(22, &input, &output);
-    pop(&output);
-    pop(&output);
-    pop(&output);
-    push(33, &input, &output);
-    push(44, &input, &output);
-    push(55, &input, &output);
-    push(66, &input, &output);
-    pop(&output);
-    push(77, &input, &output);
-    push(88, &input, &output);
-    push(99, &input, &output);
-    push(111, &input, &output);
+    push(11, &top);
+    push(22, &top);
+    pop(&top);
+    pop(&top);
+    pop(&top);
+    push(33, &top);
+    push(44, &top);
+    push(55, &top);
+    push(66, &top);
+    pop(&top);
+    push(77, &top);
+    push(88, &top);
+    push(99, &top);
+    push(111, &top);
   
     return 0;
 }
 
 
-void push(int data, node** inputAddr, node** outputAddr)
+void push(int data, node** topAddr)
 {
-//    node* input = *inputAddr;
-    node* output = *outputAddr;
-    
+    node* top = *topAddr;
     node* newNode = (node*)malloc(sizeof(node));
     newNode->nData = data;
-    newNode->pNext = NULL;
+    newNode->pPrev = NULL;
     
     
-    if (currentQueueSize >= QUEUE_SIZE)
+    
+    if (currentStackSize >= STACK_SIZE)
     {
-        cout << "push(" << data << "): overflow!" << endl;
+        cout << "push(" << data << "): " << "overflow! " << endl;
         return;
     }
-    else if (currentQueueSize <= 0) // 새 노드가 큐의 첫 노드라면,
+    else if(currentStackSize == 0)
     {
-        *outputAddr = newNode;
-        *inputAddr = newNode;
-      
-        currentQueueSize++;
-        cout << "push(" << data << "), currentQueueSize: " << currentQueueSize << endl;
+        *topAddr = newNode;
+        currentStackSize++;
     }
     else
     {
-        output->pNext = newNode;
-        *inputAddr = newNode;
-        
-        currentQueueSize++;
-        cout << "push(" << data << "), currentQueueSize: " << currentQueueSize << endl;
+        newNode->pPrev = top;
+        *topAddr = newNode;
+        currentStackSize++;
     }
+    
+    cout << "push(" << data << "), " << "stackSize: " << currentStackSize << endl;
+    
 }
 
 
-int pop(node** outputAddr)
+int pop(node** topAddr)
 {
+    node* top = *topAddr;
     int data = 0;
-    node* output = *outputAddr;
-    
-    if (currentQueueSize <= 0)
+    if (currentStackSize <= 0)
     {
         cout << "pop(): underflow! " << endl;
         return 0;
     }
     else
     {
-        data = output->nData;
-        *outputAddr = output->pNext;
-        currentQueueSize--;
-        cout << "pop(): " << data << " currentQueueSize: " << currentQueueSize << endl;
-        free(output);
+        data = top->nData;
+        *topAddr = top->pPrev;
+        currentStackSize--;
+        cout << "pop(): " << data << " stackSize: " << currentStackSize << endl;
+        
+        return data;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    return data;
     
 }
