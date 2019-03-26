18113df2666a90db7855a8c2a3b5d245e10429a0
JayLee2017 <dlwodls10@naver.com>
2019-03-27 04:23:59 +0900

    Add files via upload

---
+++ circle, triangle, rectangle.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  dailyPracticeForC++1
+//
+//  Created by Jay on 10/12/2018.
+//  Copyright © 2018 Jay. All rights reserved.
+//
+
+#include <iostream>
+
+typedef struct coordinate {
+    float x;
+    float y;
+}coordinate;
+
+class myCircle {
+    float radius;
+    coordinate center;
+    
+
+};
+
+class myTriangle {
+    coordinate point[3];
+    // 겹치는 좌표 입력시 에러 출력하게 만드는 기능 구현.
+    
+    float triangleArea; // 세 점중 x최소값과 x최대값, y최소값과 y최대값을 구해서 입력과 동시에 넓이가 자동으로 구해지게 하는 기능 구현.
+
+};
+
+class myRectangle {
+    coordinate point1;
+    coordinate point2;
+
+    
+};
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+ 
+    
+    
+    
+    
+    return 0;
+}
